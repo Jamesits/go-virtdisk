c32e940497dfb269b7e8d2589dea15f3e15e4758
James Swineson <github@public.swineson.me>
2023-03-05 23:52:24 +0800

    update some structs

--- pkg/virtdisk/structs.go
+++ pkg/virtdisk/structs.go
@@ -6,7 +6,7 @@ package virtdisk
 // Types conversion:
 // - ULONG -> uint32
 // - ULONGLONG -> uint64
-// - PCWSTR (const wchar_t*) -> uintptr (should be []uint16, but...)
+// - PCWSTR (const wchar_t*) -> *uint16 (should be []uint16, but...)
 
 import "github.com/google/uuid"
 
@@ -51,8 +51,8 @@ type CreateVirtualDiskParametersV1 struct {
 	MaximumSize       uint64
 	BlockSizeInBytes  uint32
 	SectorSizeInBytes uint32
-	ParentPath        uintptr
-	SourcePath        uintptr
+	ParentPath        *uint16
+	SourcePath        *uint16
 }
 
 type CreateVirtualDiskParametersV2 struct {
@@ -62,10 +62,98 @@ type CreateVirtualDiskParametersV2 struct {
 	BlockSizeInBytes          uint32
 	SectorSizeInBytes         uint32
 	PhysicalSectorSizeInBytes uint32
-	ParentPath                uintptr
-	SourcePath                uintptr
+	ParentPath                *uint16
+	SourcePath                *uint16
 	OpenFlags                 OpenVirtualDiskFlag
 	ParentVirtualStorageType  VirtualStorageType
 	SourceVirtualStorageType  VirtualStorageType
 	ResiliencyGuid            uuid.UUID
+}
+
+type DeleteSnapshotVhdSetParametersV1 struct {
+	Version
+	SnapshotId uuid.UUID
+}
+
+type ExpandVirtualDiskParametersV1 struct {
+	Version
+	NewSize uint64
+}
+
+type GetVirtualDiskInfoV1 struct {
+	Version
+
+	VirtualSize  uint64
+	PhysicalSize uint64
+	BlockSize    uint32
+	SectorSize   uint32
+
+	Identifier uuid.UUID
+
+	ParentResolved       bool
+	ParentLocationBuffer *uint16
+
+	ParentIdentifier   uuid.UUID
+	ParentTimestamp    uint32
+	VirtualStorageType VirtualStorageType
+	ProviderSubtype    uint32
+	Is4kAligned        bool
+	IsLoaded           bool
+
+	LogicalSectorSize  uint32
+	PhysicalSectorSize uint32
+	IsRemote           bool
+
+	VhyPhysicalSectorSize   uint32
+	SmallestSafeVirtualSize uint64
+	FragmentationPercentage uint32
+	VirtualDiskId           uuid.UUID
+
+	Enabled      bool
+	NewerChanges bool
+	MostRecentId *uint16
+}
+
+type MergeVirtualDiskParametersV1 struct {
+	Version
+	MergeDepth uint32
+}
+
+type MergeVirtualDiskParametersV2 struct {
+	Version
+	MergeSourceDepth uint32
+	MergeTargetDepth uint32
+}
+
+type MirrorVirtualDiskParametersV1 struct {
+	Version
+	MirrorVirtualDiskPath *uint16
+}
+
+type ModifyVhdSetParametersV1 struct {
+	Version
+
+	SnapshotId       uuid.UUID
+	SnapshotFilePath *uint16
+
+	DefaultSnapshotId uuid.UUID
+	DefaultFilePath   *uint16
+}
+
+type OpenVirtualDiskParametersV1 struct {
+	Version
+	RWDepth uint32
+}
+
+type OpenVirtualDiskParametersV2 struct {
+	Version
+	GetInfoOnly    bool
+	ReadOnly       bool
+	ResiliencyGuid uuid.UUID
+}
+
+type QueryChangesVirtualDiskRange struct {
+	ByteOffset uint64
+	ByteLength uint64
+	Reserved   uint64
 }
