ae24fccd99d5b4adbc346b4be56221fe36313f49
James Swineson <github@public.swineson.me>
2023-03-12 14:37:25 +0800

    struct definition seems to be done

--- pkg/virtdisk/structs.go
+++ pkg/virtdisk/structs.go
@@ -8,17 +8,14 @@ package virtdisk
 // - ULONGLONG -> uint64
 // - PCWSTR (const wchar_t*) -> *uint16 (should be []uint16, but...)
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+)
 
 // common
 
 type Version struct {
 	Version uint16
-}
-
-type VirtualStorageType struct {
-	DeviceId uint32
-	VendorId uuid.UUID
 }
 
 // specific
@@ -82,33 +79,82 @@ type ExpandVirtualDiskParametersV1 struct {
 
 type GetVirtualDiskInfoV1 struct {
 	Version
-
 	VirtualSize  uint64
 	PhysicalSize uint64
 	BlockSize    uint32
 	SectorSize   uint32
+}
 
+type GetVirtualDiskInfoV2 struct {
+	Version
 	Identifier uuid.UUID
+}
 
+type GetVirtualDiskInfoV3 struct {
+	Version
 	ParentResolved       bool
 	ParentLocationBuffer *uint16
+}
 
-	ParentIdentifier   uuid.UUID
-	ParentTimestamp    uint32
+type GetVirtualDiskInfoV4 struct {
+	Version
+	ParentIdentifier uuid.UUID
+}
+
+type GetVirtualDiskInfoV5 struct {
+	Version
+	ParentTimestamp uint32
+}
+
+type GetVirtualDiskInfoV6 struct {
+	Version
 	VirtualStorageType VirtualStorageType
-	ProviderSubtype    uint32
-	Is4kAligned        bool
-	IsLoaded           bool
+}
 
+type GetVirtualDiskInfoV7 struct {
+	Version
+	ProviderSubtype uint32
+}
+
+type GetVirtualDiskInfoV8 struct {
+	Version
+	Is4kAligned bool
+}
+
+type GetVirtualDiskInfoV9 struct {
+	Version
+	IsLoaded bool
+}
+
+type GetVirtualDiskInfoV10 struct {
+	Version
 	LogicalSectorSize  uint32
 	PhysicalSectorSize uint32
 	IsRemote           bool
+}
 
-	VhyPhysicalSectorSize   uint32
+type GetVirtualDiskInfoV11 struct {
+	Version
+	VhyPhysicalSectorSize uint32
+}
+
+type GetVirtualDiskInfoV12 struct {
+	Version
 	SmallestSafeVirtualSize uint64
-	FragmentationPercentage uint32
-	VirtualDiskId           uuid.UUID
+}
 
+type GetVirtualDiskInfoV13 struct {
+	Version
+	FragmentationPercentage uint32
+}
+
+type GetVirtualDiskInfoV14 struct {
+	Version
+	VirtualDiskId uuid.UUID
+}
+
+type GetVirtualDiskInfoV15 struct {
+	Version
 	Enabled      bool
 	NewerChanges bool
 	MostRecentId *uint16
@@ -173,4 +219,92 @@ type RawScsiVirtualDiskParametersV1 struct {
 
 type RawScsiVirtualDiskResponseV1 struct {
 	Version
+	ScsiStatus         uint8
+	SenseInfoLength    uint8
+	DataTransferLength uint32
+}
+
+type ResizeVirtualDiskParametersV1 struct {
+	Version
+	NewSize uint64
+}
+
+type SetVirtualDiskInfoV1 struct {
+	Version
+	ParentFilePath *uint16
+}
+
+type SetVirtualDiskInfoV2 struct {
+	Version
+	UniqueIdentifier uuid.UUID
+}
+
+type SetVirtualDiskInfoV3 struct {
+	Version
+	ChildDepth     uint32
+	ParentFilePath *uint16
+}
+
+type SetVirtualDiskInfoV4 struct {
+	Version
+	VhdPhysicalSectorSize uint32
+}
+
+type SetVirtualDiskInfoV5 struct {
+	Version
+	VirtualDiskId uuid.UUID
+}
+
+type SetVirtualDiskInfoV6 struct {
+	Version
+	ChangeTrackingEnabled bool
+}
+
+type SetVirtualDiskInfoV7 struct {
+	Version
+	LinkageId      uuid.UUID
+	ParentFilePath *uint16
+}
+
+type StorageDependencyInfoV1 struct {
+	Version
+	NumberEntries   uint32
+	Version1Entries []*StorageDependencyInfoType1
+}
+
+type StorageDependencyInfoType1 struct {
+	DependencyTypeFlags   DependentDiskFlag
+	ProviderSpecificFlags uint32
+	VirtualStorageType    VirtualStorageType
+}
+
+type StorageDependencyInfoV2 struct {
+	Version
+	NumberEntries   uint32
+	Version2Entries []*StorageDependencyInfoType2
+}
+
+type StorageDependencyInfoType2 struct {
+	StorageDependencyInfoType1
+	AncestorLevel               uint32
+	DependencyDeviceName        *uint16
+	HostVolumeName              *uint16
+	DependentVolumeName         *uint16
+	DependentVolumeRelativePath *uint16
+}
+
+type TakeSnapshotVhdSetParametersV1 struct {
+	Version
+	SnapshotId uuid.UUID
+}
+
+type VirtualDiskProgress struct {
+	OperationStatus uint32 // windows.Errno
+	CurrentValue    uint64
+	CompletionValue uint64
+}
+
+type VirtualStorageType struct {
+	DeviceId uint32
+	VendorId uuid.UUID
 }
